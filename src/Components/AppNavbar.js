d82e8e2536c64d7eddc701b04869630ededa2173
Berk Tenkoğlu <berk.tenkoglu@gmail.com>
2024-07-13 18:23:56 +0300

    Added React-icons

--- src/Components/AppNavbar.js
+++ src/Components/AppNavbar.js
@@ -6,29 +6,32 @@ import Logo from "../Assets/Logo.jpg"
 import Doctor from "../Assets/senior-woman-doctor-and-portrait-smile-for-health-2023-11-27-05-18-16-utc.jpg";
 import More from "../Assets/more.jpg";
 import Settings from "../Assets/settings.jpg";
+import { FaUser } from "react-icons/fa";
+import { CiSettings } from "react-icons/ci";
+import { BsThreeDotsVertical } from "react-icons/bs";
+import { PiSketchLogoLight } from "react-icons/pi";
 
 function AppNavbar() {
   return (
     <Navbar >
     <Container style={{ display: 'flex', justifyContent: 'space-between' }}>
       <Navbar.Brand href="/home">
-        <img src={Logo} width={"50%"} ></img>
+      <PiSketchLogoLight color='gold' size={70} />
       </Navbar.Brand>
       <Nav variant='pills'>
         <Nav.Link href="/">Overview</Nav.Link>
-        <Nav.Link href="/patients">Patients</Nav.Link>
         <Nav.Link href="/schedule">Schedule</Nav.Link>
         <Nav.Link href="/message">Message</Nav.Link>
         <Nav.Link href="/transactions">Transactions</Nav.Link>
       </Nav>
-      <Nav  style={{ display: 'flex', justifyContent: 'space-between' }}>
-        <img src={Doctor} ></img>
-        <div>
-          <b>Dr. Jose Simmons</b>
-          <p>General Practitioner</p>
+      <Nav  style={{ display: 'flex', justifyContent: 'space-between', alignItems:"center" }}>
+        <FaUser size={28} className='mx-1'/>
+        <div className='mx-1'>
+          <b>Username</b>
+          <p>User Title</p>
           </div>
-          <img src={Settings} height={"30px"}></img>
-          <img src={More} height={"30px"}></img>
+          <CiSettings className='mx-1' size={28}/>
+          <BsThreeDotsVertical size={28} />
       </Nav>
     </Container>
   </Navbar>
